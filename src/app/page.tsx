46995872884eecd54f2b64f0c210aeaea2eabe80
sebas <seba@example.com>
2023-06-28 10:25:18 -0400

    add next_url to replace localhost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,24 @@ import FormSearch from "@/components/FormSearch";
 import { useState } from "react";
 import Book from "@/interfaces/book";
 
+export const NEXT_URL =
+  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+
 const Home = () => {
   const [book, setBook] = useState<Book | null>(null);
   const [bookFeriaChilena, setBookFeriaChilena] = useState<Book | null>(null);
   const [bookAntartica, setBookAntartica] = useState<Book | null>(null);
 
   const getBook = async (isbn: string) => {
-    const res = await fetch(`http://localhost:3000/api/search?isbn=${isbn}`);
+    const res = await fetch(`${NEXT_URL}/api/search?isbn=${isbn}`);
     const data = await res.json();
     setBook(data);
 
-    const resFeria = await fetch(`http://localhost:3000/api/search/feriachilena?isbn=${isbn}`);
+    const resFeria = await fetch(`${NEXT_URL}/api/search/feriachilena?isbn=${isbn}`);
     const dataFeria = await resFeria.json();
     setBookFeriaChilena(dataFeria);
 
-    const resAntartica = await fetch(`http://localhost:3000/api/search/antartica?isbn=${isbn}`);
+    const resAntartica = await fetch(`${NEXT_URL}/api/search/antartica?isbn=${isbn}`);
     const dataAntartica = await resAntartica.json();
     setBookAntartica(dataAntartica);
   };
