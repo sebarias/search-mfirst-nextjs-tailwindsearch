d990e05e8443f4f6979e01d24b236039a088efb9
sebas <seba@example.com>
2023-07-01 09:09:34 -0400

    cambio de enpoints a gcp functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import FormSearch from "@/components/FormSearch";
 import { useState } from "react";
 import Book from "@/interfaces/book";
 
-const apiUrl = process.env.API_URL;
+//const apiUrl = process.env.API_URL;
+const apiUrl = "http://localhost:3000";
 
 const Home = () => {
   const [book, setBook] = useState<Book | null>(null);
@@ -14,25 +15,26 @@ const Home = () => {
 
   const getBook = async (isbn: string) => {
 
-    console.log(process.env.URL_V, ", ", process.env.VERCEL_URL, process.env.VERCEL_BRANCH_URL,process.env.NEXT_PUBLIC_VERCEL_URL,process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL )
-    const res = await fetch(`${apiUrl}/api/search?isbn=${isbn}`);
+    //const res = await fetch(`${apiUrl}/api/search?isbn=${isbn}`);
+    const res = await fetch(`https://search-book-buscalibre-function-vwp6agkcra-uc.a.run.app?isbn=${isbn}`);
     const data = await res.json();
-    setBook(data);
+    setBook(data.data);
 
-    const resFeria = await fetch(`${apiUrl}/api/search/feriachilena?isbn=${isbn}`);
-    const dataFeria = await resFeria.json();
-    setBookFeriaChilena(dataFeria);
+    // const resFeria = await fetch(`${apiUrl}/api/search/feriachilena?isbn=${isbn}`);
+    // const dataFeria = await resFeria.json();
+    // setBookFeriaChilena(dataFeria); 
 
-    const resAntartica = await fetch(`${apiUrl}/api/search/antartica?isbn=${isbn}`);
+    //const resAntartica = await fetch(`${apiUrl}/api/search/antartica?isbn=${isbn}`);
+    const resAntartica = await fetch(`https://search-book-antartica-function-vwp6agkcra-uc.a.run.app?isbn=${isbn}`);
     const dataAntartica = await resAntartica.json();
-    setBookAntartica(dataAntartica);
+    setBookAntartica(dataAntartica.data);
   };
 
   return (
     <>
       <FormSearch getBook={getBook} />
       {book && <BookCardInfo book={book} />}
-      {bookFeriaChilena && <BookCardInfo book={bookFeriaChilena} />}
+     {/* {bookFeriaChilena && <BookCardInfo book={bookFeriaChilena} />} */}
       {bookAntartica && <BookCardInfo book={bookAntartica} />}
     </>
   );
