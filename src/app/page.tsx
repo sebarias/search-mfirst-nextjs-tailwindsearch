09bd1d104cff00010bc4cbcfbeaccecae2feb1ed
sebas <seba@example.com>
2023-07-06 12:19:48 -0400

    fix split image url issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,30 +12,45 @@ const Home = () => {
   const [book, setBook] = useState<Book | null>(null);
   const [bookFeriaChilena, setBookFeriaChilena] = useState<Book | null>(null);
   const [bookAntartica, setBookAntartica] = useState<Book | null>(null);
+  const [booksMercadoLibre, setBooksMercadoLibre] = useState<Book[] | null>(
+    null
+  );
 
   const getBook = async (isbn: string) => {
-
     //const res = await fetch(`${apiUrl}/api/search?isbn=${isbn}`);
-    const res = await fetch(`https://search-book-buscalibre-function-vwp6agkcra-uc.a.run.app?isbn=${isbn}`);
+    const res = await fetch(
+      `https://search-book-buscalibre-function-vwp6agkcra-uc.a.run.app?isbn=${isbn}`
+    );
     const data = await res.json();
     setBook(data.data);
 
     // const resFeria = await fetch(`${apiUrl}/api/search/feriachilena?isbn=${isbn}`);
     // const dataFeria = await resFeria.json();
-    // setBookFeriaChilena(dataFeria); 
+    // setBookFeriaChilena(dataFeria);
 
     //const resAntartica = await fetch(`${apiUrl}/api/search/antartica?isbn=${isbn}`);
-    const resAntartica = await fetch(`https://search-book-antartica-function-vwp6agkcra-uc.a.run.app?isbn=${isbn}`);
+    const resAntartica = await fetch(
+      `https://search-book-antartica-function-vwp6agkcra-uc.a.run.app?isbn=${isbn}`
+    );
     const dataAntartica = await resAntartica.json();
     setBookAntartica(dataAntartica.data);
+
+    const resMercadoLibre = await fetch(
+      `https://search-book-mercadolibre-function-vwp6agkcra-uc.a.run.app?isbn=${isbn}`
+    );
+    const dataMercadoLibre = await resMercadoLibre.json();
+    setBookAntartica(dataMercadoLibre.data);
   };
 
   return (
     <>
       <FormSearch getBook={getBook} />
       {book && <BookCardInfo book={book} />}
-     {/* {bookFeriaChilena && <BookCardInfo book={bookFeriaChilena} />} */}
+      {/* {bookFeriaChilena && <BookCardInfo book={bookFeriaChilena} />} */}
       {bookAntartica && <BookCardInfo book={bookAntartica} />}
+      {booksMercadoLibre?.map((book)=>{
+        console.log(book)
+      }) }
     </>
   );
 };
